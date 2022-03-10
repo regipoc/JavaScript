9ce1705636b4f7e92ce83cfda26b87dbaeb55d21
regipoc <regipoc@gmail.com>
2022-03-10 22:05:45 +0200

    JavasCript pamoka

---
+++ index.js
@@ -0,0 +1,31 @@
+//Skaičiai
+//1+2
+let naujiMetai=25;
+
+naujiMetai=naujiMetai + 5;
+//yra tas pats
+naujiMetai+=5;
+let metai=2022;
+let senesniMetai=2000;
+let skirtumas=metai-senesniMetai;
+// du kintamieji su skaičiais
+let dienos = 30;
+let savaites = 4;
+console.log('Hello world');
+let skaičius =20;
+kitasSkaičius = 7;
+let liekana = skaičius % kitasSkaičius
+console.log(skaičius % kitasSkaičius)
+console.log(liekana)
+let name = 'Regina';
+let othername = 'Pociuviene';
+let gimimometai = '1977';
+let dabartiniaimetai = 2022;
+atsakymas = `${name} ${othername} man yra ${dabartiniaimetai - gimimometai}`
+console.log(atsakymas);
+let kitasVardas = 'Mano vardas Yra';
+console.log(kitasVardas.charAt(5));
+
+
+
+
