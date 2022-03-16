57fd711d9a40329090c7aa7ae89ca9801587a08f
regipoc <regipoc@gmail.com>
2022-03-16 21:59:51 +0200

    funkcija

--- index.js
+++ index.js
@@ -153,24 +153,120 @@ const skaic3 =[]
    // console.log(i)
  // }
 
- const todoList = [
-    'Pradeti nuo q komandos', 
-    'panaudoti prompt, kad gauti duomenis', 
-    'panaudoti simbolius ir console atskirti vienai uzduociai nuo kitos'
-    ];
-    const todoList = [
-      'Pradeti nuo q komandos', 
-      'panaudoti prompt, kad gauti duomenis', 
-      'panaudoti simbolius ir console atskirti vienai uzduociai nuo kitos'
-      ];
-    
-    
-    let vartotojoIvestaKomanda = prompt('Kokia yra jusu sekanti komanda')
-    
-    while(vartotojoIvestaKomanda !== 'quit') {
-      if (vartotojoIvestaKomanda === 'list') {
-        vartotojoIvestaKomanda = prompt('Iveskite nauja darba i darbu sarasa')
-        todoList.push(vartotojoIvestaKomanda)
-      } else if ()
-      vartotojoIvestaKomanda = prompt('Kokia yra jusu sekanti komanda')
-    }  
+ const todoList = ["Pradeti nuo q komandos", 
+  "panaudoti prompt, kad gauti duomenis", 
+  "panaudoti simbolius ir console atskirti vienai uzduociai nuo kitos"
+  ];
+let vartotojoIvestaKomanda = prompt("Kokia yra jusu sekanti komanda")
+while(vartotojoIvestaKomanda !== "quit") {
+  if (vartotojoIvestaKomanda === "new") {
+    let naujasDarbas = prompt("Iveskite nauja darba i darbu sarasa");
+    todoList.push(naujasDarbas);
+    vartotojoIvestaKomanda = prompt("Kokia yra jusu sekanti komanda");
+  } else if (vartotojoIvestaKomanda === "list") {
+    console.log(`Jusu darbu sarasas:`);
+    for (let i = 0; i < todoList.length; i++) {
+      console.log(`----------------------`);
+      console.log(`${i} - ${todoList[i]}`);
+      console.log(`----------------------`);
+    } 
+    vartotojoIvestaKomanda = prompt("Kokia yra jusu sekanti komanda");
+  } else if (vartotojoIvestaKomanda === 'delete') {
+    let index = parseInt(prompt("Iveskite norimo istrinti darbo numeri"));
+    todoList.splice(index, 1)
+    vartotojoIvestaKomanda = prompt("Kokia yra jusu sekanti komanda")
+  } else {
+    vartotojoIvestaKomanda = "quit"
+    console.log("Ivesta neteisinga komanda")
+  }
+}
+//FUNKCIJOS
+function duSkaiciai(skaicius, laipsnis) {
+  console.log(skaicius ** laipsnis)
+  duSkaiciai(5, 4)
+
+  function atiduotiDidesni(a, b){
+    //if (a >b){
+    // console.log(a)
+    //}elseif (a < b){
+    //   console.log(b)
+    //} else {
+    //  console.log("skaicia yra lygus")
+    //}
+    console.log(Math.max(a,b))
+  }
+}
+  
+// kokiaTaiDiena(8)
+// Parasyti funkcija kuri priims dienos numeri 1, 2, ... 7
+// ivedus 1 funkcija turetu isspausdinti pirmadienis 
+// 2 - antradienis ir t.t.
+function kokiaTaiDiena(num){
+  if (num === 1) {
+    console.log('Siandien yra pirmadienis');
+  } else if (num === 2) {
+    console.log('Siandien yra antradienis');
+  } else {
+    console.log('Savaiteje tokios dienos nera')
+  }
+}
+console.log (getDay)
+
+//Uzduotis susumuoti visus skaicius masyve. 
+const sudetiVisusSkaicius = [12, 4, 16, 3, 8, 9, 'hi', 12, 2, 1, 5, 3, 9, 55, 292, 13, undefined]
+
+function sudetiVisusMasyvoSkaicius(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if(Number.isInteger(arr[i])) {
+      sum = sum + arr[i]
+    }
+  }
+  console.log(sum)
+}
+
+sudetiVisusMasyvoSkaicius(sudetiVisusSkaicius)
+
+// uzduotis parasyti funckija kuri startini ir galini skaiciu
+// musu atveju naudosime skaicius nuo 0 iki 100
+// susikurti masyva su skaiciais nuo 0 iki 100 nerasant viso masyvo ranka
+// ties kiekvienu skaiciu, kuris dalinasi is 3 be liekanos
+// parasys 'Triju kartotinis
+// tas pats su kiekvienu kuris dalinasi is 5
+// "Penkiu kartotinis"
+// jei skaicius dalinasi is 3 ir is 5 be liekanos 
+// 'Koks tai skaicius - triju ir penkiu kartotinis"
+// galiausiai i konsole isvesti tris masyvus kuriuose bus
+// triju, penkiu bei triju ir penkiu kartotiniai
+function kartotiniai(startinisSkaicius, galinisSkaicius){
+  // const skaiciai = Array.from(Array(100).keys());
+  const skaiciai = [];  
+  const trijuKartotiniai = [];
+  const penkiuKartotiniai = [];
+  const trijuIrPenkiuKartotiniai = [];
+  for (let i = startinisSkaicius; i <= galinisSkaicius; i++){
+    skaiciai.push(i)
+  }
+
+  if (15 % 5 == 0) {
+    console.log('hi')
+  }
+
+  for (let i = 0; i < skaiciai.length; i++) {
+    if (skaiciai[i] % 3 === 0 && skaiciai[i] % 5 === 0) {
+      trijuIrPenkiuKartotiniai.push(skaiciai[i])
+      // trijuKartotiniai.push(skaiciai[i]);
+      // penkiuKartotiniai.push(skaiciai[i]);
+    } else if (skaiciai[i] % 3 === 0) {
+      trijuKartotiniai.push(skaiciai[i]);
+    } else if (skaiciai[i] % 5 === 0) {
+      penkiuKartotiniai.push(skaiciai[i]);
+    } 
+  }
+  console.log('triju', trijuKartotiniai)
+  console.log('penkiu', penkiuKartotiniai)
+  console.log('bendri', trijuIrPenkiuKartotiniai)
+}
+kartotiniai(0, 100)
+
+const learnmore
